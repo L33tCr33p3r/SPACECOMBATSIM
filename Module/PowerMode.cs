112d061c60bc9d65f590be349551c2097037d548
L33tCr33p3r <coryvin@thebandit.org>
2023-01-13 21:18:49 -0700

    Added Generator PowerMode

--- Module/PowerMode.cs
+++ Module/PowerMode.cs
@@ -6,6 +6,8 @@
 
 	public record Powered : PowerMode;
 
+	public record Generator(int Generation) : PowerMode;
+
 	public record FixedDrain(int Drain) : Powered;
 
 	public record DynamicDrain(int MinDrain, int MaxDrain) : Powered;
