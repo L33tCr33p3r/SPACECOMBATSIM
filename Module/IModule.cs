c694d168e529dc0de217fabd48342279c8a58178
L33tCr33p3r <coryvin@thebandit.org>
2023-01-15 00:06:53 -0700

    Added Connect method (and default implementation) to IModule

--- Module/IModule.cs
+++ Module/IModule.cs
@@ -7,5 +7,23 @@ namespace SpaceCombatSim.Module
 		public int ArmorClass { get; set; }
 
 		public List<IModule> Connections { get; set; }
+
+		/// <summary>
+		/// Connect to another module.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <exception cref="ArgumentException"></exception>
+		public void Connect(IModule target)
+		{
+			if (target == this)
+				throw new ArgumentException("Tried to connect a Module to itself!");
+			else if (Connections.Contains(target) && target.Connections.Contains(this))
+				throw new ArgumentException("Tried to connect to an already connected Module!");
+			else if (Connections.Contains(target) || target.Connections.Contains(this))
+				throw new ArgumentException("Tried to connect with a module that has a connection in a partial state, where the connection does not exist both ways.");
+
+			Connections.Add(target);
+			target.Connections.Add(this);
+		}
 	}
 }
